481f5a429a0e8d3b6dd5d08c2630a7f0fcbbbc71
unknown <phani_golla@yahoo.com>
2024-01-21 20:31:23 -0500

    Angular Application Hotel App

---
+++ src/app/models/reservation.ts
@@ -0,0 +1,8 @@
+export interface Reservation {
+    id: string,
+    checkInDate: Date,
+    checkOutDate: Date,
+    guestName: Date,
+    guestEmail: string,
+    roomNumber: number,
+}
---
+++ src/app/reservation-form/reservation-form.component.ts
@@ -0,0 +1,53 @@
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
+import { Router,ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-reservation-form',
+  templateUrl: './reservation-form.component.html',
+  styleUrls: ['./reservation-form.component.css']
+})
+export class ReservationFormComponent implements OnInit {
+ 
+  reservationForm: FormGroup = new FormGroup({});
+  
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required]
+    })
+
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id) { 
+      this.reservationService.getReservation(id).subscribe(reservation => { 
+        if (reservation) { 
+          this.reservationForm.patchValue(reservation);
+        }
+      });
+    }
+  }
+
+  constructor(private formBuilder: FormBuilder,private reservationService: ReservationService,private router: Router,private activatedRoute: ActivatedRoute) { 
+
+  }
+  
+  onSubmit() { 
+    if (this.reservationForm.valid) { 
+      let reservation: Reservation = this.reservationForm.value;
+      
+      let id = this.activatedRoute.snapshot.paramMap.get('id');  
+      if (id) {
+        this.reservationService.updateReservation(id,reservation).subscribe(() => console.log("Reservation updated successfully..."));
+      }else{ 
+        this.reservationService.addReservation(reservation).subscribe(() => console.log("Reservation posted succesfully"));
+      }
+
+      this.router.navigate(['/list']);
+    }
+  }
+}
